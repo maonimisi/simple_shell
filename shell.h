579b1ba5054392d7b7f9bad9f965e8838c7bc8bc
maonimisi <mabdulrasheed1210@gmail.com>
2023-05-20 03:34:35 -0700

    simple shell

--- shell.h
+++ shell.h
@@ -1,5 +1,5 @@
-#ifndef SHELL_H_
-#define SHELL_H_
+#ifndef SHELL_H
+#define SHELL_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,16 +12,109 @@
 #include <fcntl.h>
 #include <errno.h>
 
+/*read/write buffers */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+/* 1 if using system getline() */
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
+
+/*Command chaining */
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND		2
+#define CMD_CHAIN	3
+
+/*Convert_number() */
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+/* 1 if using system getline() */
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+/**
+ * struct liststr - singly linked list
+ * @num: the number field
+ * @str: a string
+ * @next: points to the next node
+ */
+typedef struct liststr
+{
+	int num;
+	char *str;
+	struct liststr *next;
+} list_t;
+
+/**
+ * struct program_info - contains information related to a program execution
+ * @_args: String generated from getline containing program arguments
+ * @_argv: Array of strings generated from arguments
+ * @_path: A string representing the path of the current command
+ * @_argc: Count of arguments
+ * @_lineCount: Line count of the program execution
+ * @_errorNumber: Error code for exit functions
+ * @_countLine: Flag indicating whether to count this line of input
+ * @_filename: Name of the program file
+ * @_envp: linked list representing the local copy of the environment variables
+ * @_modifiedEnvp: Custom modified copy of the environment variables from the linked list
+ * @_history: History linked list node
+ * @_alias: Alias linked list node
+ * @_changedEnvp: Flag indicating if the environment was changed
+ * @_status: Return status of the last executed command
+ * @_commandBuffer: Address of the pointer to the command buffer used for chaining commands
+ * @_commandBufferType: Type of command buffer (CMD_type: ||, &&, ;)
+ * @_readFileDescriptor: File descriptor used for reading line input
+ * @_historyCount: Count of history line numbers
+ */
+typedef struct program_info
+{
+	char *_args;
+	char **_argv;
+	char *_path;
+	int _argc;
+	unsigned int _lineCount;
+	int _errorNumber;
+	int _countLine;
+	char *_filename;
+	list_t *_envp;
+	list_t *_history;
+	list_t *_alias;
+	char **_modifiedEnvp;
+	int _changedEnvp;
+	int _status;
+	char **_commandBuffer;
+	int _commandBufferType;
+	int _readFileDescriptor;
+	int _historyCount;
+} info_t;
+
+/**
+ *struct builtin - contains a builtin string and related function
+ *@type: the builtin command flag
+ *@func: the function
+ */
+typedef struct builtin
+{
+	char *type;
+	int (*func)(info_t *);
+} builtin_table;
+
+
 /*_atoi.c*/
 int _isInteractive(info_t *info);
-int _isdelimeter(char c, char *delimeter);
+int _isDelimeter(char c, char *delimeter);
 int _isAlpha(int c);
 int _atoi(char *s);
-
-/*_string3.c*/
-char *_strncpy(char *destination, char *source, int max_len);
-char *_strncat(char *destination, char *source, int max_len);
-char *_strchr(char *str, char character);
 
 /*_error1.c*/
 int _eputchar(char c);
@@ -34,6 +127,7 @@ int _erratoi(char *str);
 int _printDecimal(int input, int fd);
 void _printError(info_t *info, char *error_type);
 void _removeComments(char *buf);
+char *_convertNumber(long int, int, int);
 
 /*_builtin1.c*/
 int _myExit(info_t *info);
@@ -78,7 +172,6 @@ int _writeHistory(info_t *info);
 int _renumberHistory(info_t *info);
 int _readHistory(info_t *info);
 int _addHistoryEntry(info_t *info, char *buffer, int line_count);
-int _renumberHistory(info_t *info);
 
 /*_string1.c*/
 int _strlen(char *s);
@@ -91,6 +184,11 @@ char *_strcpy(char *dest, char *src);
 char *_strdup(char *str);
 void _puts(char *str);
 int _putchar(char c);
+
+/*_string3.c*/
+char *_strncpy(char *destination, char *source, int max_len);
+char *_strncat(char *destination, char *source, int max_len);
+char *_strchr(char *str, char character);
 
 /*_realloc.c*/
 char *_memset(char *s, char b, unsigned int n);
@@ -112,10 +210,15 @@ size_t _printList(const list_t *h);
 list_t *_nodeStartsWith(list_t *node, char *prefix, char c);
 ssize_t _getNodeIndex(list_t *head, list_t *node);
 
-/*_path.c*/
+/*_path1.c*/
 int _isExecutableCommand(info_t *info, char *path);
 char *_duplicateCharacters(char *source, int start, int end);
 char *_findCommandPath(info_t *info, char *path_string, char *command);
+
+/*_path2.c*/
+int _findBuiltin(info_t *info);
+void _findCommand(info_t *info);
+void _forkCommand(info_t *info);
 
 /*_tokenize.c*/
 char **_splitString(char *str, char *delim);
@@ -129,91 +232,7 @@ int _replaceAlias(info_t *info);
 int _replaceVars(info_t *info);
 int _replaceString(char **old_str, char *new_str);
 
-/*read/write buffers */
-#define READ_BUF_SIZE 1024
-#define WRITE_BUF_SIZE 1024
-#define BUF_FLUSH -1
-
-/* 1 if using system getline() */
-#define USE_GETLINE 0
-#define USE_STRTOK 0
-
-#define HIST_FILE	".simple_shell_history"
-#define HIST_MAX	4096
-
-/**
- * struct liststr - singly linked list
- * @num: the number field
- * @str: a string
- * @next: points to the next node
- */
-typedef struct liststr
-{
-	int num;
-	char *str;
-	struct liststr *next;
-} list_t;
-
-#define INFO_INIT \
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-	0, 0, 0}
-
-/*Command chaining */
-#define CMD_NORM	0
-#define CMD_OR		1
-#define CMD_AND		2
-#define CMD_CHAIN	3
-
-/*Convert_number() */
-#define CONVERT_LOWERCASE	1
-#define CONVERT_UNSIGNED	2
-
-/* 1 if using system getline() */
-#define USE_GETLINE 0
-#define USE_STRTOK 0
-
-
-/**
- * struct program_info - contains information related to a program execution
- * @_args: String generated from getline containing program arguments
- * @_argv: Array of strings generated from arguments
- * @_path: A string representing the path of the current command
- * @_argc: Count of arguments
- * @_lineCount: Line count of the program execution
- * @_errorNumber: Error code for exit functions
- * @_countLine: Flag indicating whether to count this line of input
- * @_filename: Name of the program file
- * @_envp: linked list representing the local copy of the environment variables
- * @_modifiedEnvp: Custom modified copy of the environment variables from the linked list
- * @_history: History linked list node
- * @_alias: Alias linked list node
- * @_changedEnvp: Flag indicating if the environment was changed
- * @_status: Return status of the last executed command
- * @_commandBuffer: Address of the pointer to the command buffer used for chaining commands
- * @_commandBufferType: Type of command buffer (CMD_type: ||, &&, ;)
- * @_readFileDescriptor: File descriptor used for reading line input
- * @_historyCount: Count of history line numbers
- */
-typedef struct program_info
-{
-	char *_args;
-	char **_argv;
-	char *_path;
-	int _argc;
-	unsigned int _lineCount;
-	int _errorNumber;
-	int _countLine;
-	char *_filename;
-	list_t *_envp;
-	list_t *_history;
-	list_t *_alias;
-	char **_modifiedEnvp;
-	int _changedEnvp;
-	int _status;
-	char **_commandBuffer;
-	int _commandBufferType;
-	int _readFileDescriptor;
-	int _historyCount;
-} info_t;
+/*_execute.c*/
+int _executeCommand(info_t *info, char **argv);
 
 #endif
